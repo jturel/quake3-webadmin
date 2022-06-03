20fa83fa9fcf203ccc9af7e80628d9575f126200
Jonathon Turel <jturel@gmail.com>
2022-06-03 12:36:34 -0400

    Add PouchDB

--- src/App.js
+++ src/App.js
@@ -10,10 +10,24 @@ import {
   Routes,
 } from 'react-router-dom';
 
+import PouchDB from 'pouchdb';
+
 import CreateServer from './components/CreateServer';
 import ServerList from './components/ServerList';
 
 function App() {
+
+  const db = new PouchDB('quake3-webadmin');
+
+  db.info().then(function(info) {
+    console.log(info);
+  });
+
+  const createServer = () => {
+    //db.info();
+    return "foo";
+  };
+
   return (
     <BrowserRouter>
       <Flex>
@@ -30,7 +44,7 @@ function App() {
         <Box width={3/4}>
           <Routes>
             <Route path="/" element={<ServerList/>} />
-            <Route path="/servers/create" element={<CreateServer/>} />
+            <Route path="/servers/create" element={<CreateServer createServer={createServer} />} />
           </Routes>
         </Box>
       </Flex>
