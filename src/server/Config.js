3f992be202e1d4a5711d74eb477111f9d4b9ddb0
Jonathon Turel <jturel@gmail.com>
2022-06-19 11:49:53 -0400

    Fix Server tests

--- src/server/Config.js
+++ src/server/Config.js
@@ -1,6 +1,12 @@
+const os = require('os');
+
 module.exports = () => {
+  const homeDir = os.homedir();
+  const configDir = '.config';
+  const configPath = `${homeDir}/${configDir}/quake3-webadmin`;
+
   return {
-    'dbPath': process.env.Q3WEBADMIN_DB_PATH || '/home/jturel/.config/quake3-webadmin/pouchdb',
+    'dbPath': process.env.Q3WEBADMIN_DB_PATH || `${configPath}/pouchdb`,
     'baseQ3Path': '/home/jturel/.q3a/baseq3',
     'quakeExecutable': '/home/jturel/code/ioq3-main/build/release-linux-x86_64/ioq3ded.x86_64',
   };
