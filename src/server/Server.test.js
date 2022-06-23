ffb6fbe7d40c8fa905b16ec7c640edf1d59e5990
Jonathon Turel <jturel@gmail.com>
2022-06-23 02:44:58 -0400

    fix tests windows

--- src/server/Server.test.js
+++ src/server/Server.test.js
@@ -1,12 +1,15 @@
 const os = require('os');
 const tmpdir = os.tmpdir();
 const path = require('path');
-const q3Executable = path.join(__dirname, '../../mock-q3ded.sh');
+const q3Executable = path.join(
+  __dirname,
+  (process.platform === 'win32')
+    ? '../../mock-q3ded.bat'
+    : '../../mock-q3ded.sh'
+);
+
 process.env.Q3WEBADMIN_BASEQ3_PATH = tmpdir;
 process.env.Q3WEBADMIN_EXECUTABLE_PATH = q3Executable;
-
-//const fs = require('fs');
-//fs.copyFileSync('./mock-q3ded.sh', q3Executable);
 
 const { app, closeDbConnection } = require('./App');
 const supertest = require('supertest');
@@ -29,7 +32,6 @@ const createServer = () => {
 
 afterAll(async () => {
   await closeDbConnection();
-  //fs.unlinkSync(q3Executable);
 });
 
 test('lists servers', (done) => {
